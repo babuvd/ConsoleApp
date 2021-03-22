232768dfd2086d3e866cbd0be017dbd535b0f0c6
Babu V D <vd_babu@hotmail.com>
2021-03-22 20:55:38 +0530

    test

---
+++ FIleDetails.cs
@@ -0,0 +1,12 @@
+namespace conleApp
+{
+
+    public class FileDetails
+    {
+
+        public long Id { get; set; }
+
+        public string FileName { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace conleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder dr= new StringBuilder();
+            List<FileDetails> details =new  List<FileDetails>();
+
+            long id=0;
+            foreach(var dir in Directory.GetDirectories("/home/duraiv"))
+            {
+
+                details.Add(new FileDetails{
+                     Id =id,
+                     FileName=dir
+                });
+
+            }
+
+            string jsonstring= JsonSerializer.Serialize(details);
+
+
+
+            System.IO.File.WriteAllText("/home/duraiv/test.json",jsonstring);
+
+
+        }
+    }
+}
